ea837c6ab598448cd3f286d203ee9b03583031fb
shubhamkumar785 <shubhammpathak566@gmail.com>
2026-02-13 22:30:04 +0530

    add login and register page

---
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/register")
+	public String register() {
+		return "register";
+	}
+
+	
+}
