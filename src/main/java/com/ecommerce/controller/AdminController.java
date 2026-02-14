a040d424133982f266f60c8550d5d294a00629b0
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-14 18:01:35 +0530

    Add methods to load add product and category pages

--- src/main/java/com/ecommerce/controller/AdminController.java
+++ src/main/java/com/ecommerce/controller/AdminController.java
@@ -14,4 +14,13 @@ public class AdminController {
 		return "admin/index";
 	}
 	
+	@GetMapping("/loadAddProduct")
+	public String loadAddProduct() {
+		return "admin/add_product";
+	}
+	
+	@GetMapping("/category")
+	public String category() {
+		return "admin/category";
+	}
 }
