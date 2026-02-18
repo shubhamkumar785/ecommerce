2321e529aea67508f18a0f8174c621135ebda4f0
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-18 23:03:17 +0530

    Implement delete and get category by ID methods
    Added methods to delete a category by ID and retrieve a category by ID.

--- src/main/java/com/ecommerce/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/service/impl/CategoryServiceImpl.java
@@ -2,43 +2,52 @@ package com.ecommerce.service.impl;
 
 import java.util.List;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import com.ecommerce.model.Category;
 import com.ecommerce.repository.CategoryRepository;
 import com.ecommerce.service.CategoryService;
 
-
 @Service
-public class CategoryServiceImpl implements CategoryService{
+public class CategoryServiceImpl implements CategoryService {
 
+    @Autowired
+    private CategoryRepository categoryRepository;
 
+    @Override
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
 
+    @Override
+    public Boolean existCategory(String name) {
+        return categoryRepository.existsByName(name);
+    }
 
-	@Autowired 
-	private CategoryRepository categoryRepository;
-	
-	@Override
-	public Category saveCategory(Category category) {
-		return categoryRepository.save(category);
-	}
+    @Override
+    public List<Category> getAllCategory() {
+        return categoryRepository.findAll();
+    }
 
-	
-	@Override
-	public Boolean existCategory(String name) {
-		return categoryRepository.existsByName(name);
-	}
-	
+    @Override
+    public Boolean deleteCategory(int id) {
 
-	
-	@Override
-	public List<Category> getAllCategory() {
-	    return categoryRepository.findAll();
-	}
+        Category category = categoryRepository.findById(id).orElse(null);
 
+        if (!ObjectUtils.isEmpty(category)) {
+            categoryRepository.delete(category);
+            return true;
+        }
+
+        return false;
+    }
+
+    
+    @Override
+    public Category getCategoryById(int id) {
+        return categoryRepository.findById(id).orElse(null);
+    }
 
 }
-
