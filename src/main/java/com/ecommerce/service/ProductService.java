4e03d9792c65d9521c106e4f63a8fc31c6fc4175
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-24 00:52:49 +0530

    Add methods to ProductService interface

--- src/main/java/com/ecommerce/service/ProductService.java
+++ src/main/java/com/ecommerce/service/ProductService.java
@@ -1,9 +1,14 @@
 package com.ecommerce.service;
 
+import java.util.List;
+
 import com.ecommerce.model.Product;
 
 public interface ProductService {
 	
 	public Product saveProduct(Product product);
+	
+	public List<Product> getAllProduct();
 
+	public Boolean deleteProduct(Integer id);
 }
