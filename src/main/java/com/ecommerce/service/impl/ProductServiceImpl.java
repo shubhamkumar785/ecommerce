0d5878f590d72690bc35d46b858fc56dde335fa4
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-25 00:55:37 +0530

    Add methods to get and update products in service

--- src/main/java/com/ecommerce/service/impl/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/service/impl/ProductServiceImpl.java
@@ -1,10 +1,17 @@
 package com.ecommerce.service.impl;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;   // ⭐ ADD THIS
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;  
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ecommerce.model.Product;
 import com.ecommerce.repository.ProductRepository;
@@ -37,5 +44,49 @@ public class ProductServiceImpl implements ProductService {
 	    }
 
 	    return false;
+	}
+
+	@Override
+	public Product getProductById(Integer id) {
+		Product product = productRepository.findById(id).orElse(null);
+		return product;
+	}
+
+	@Override
+	public Product updateProduct(Product product, MultipartFile image) {
+		
+		Product dbProduct = getProductById(product.getId());
+		
+		String imageName = image.isEmpty() ? dbProduct.getImage() : image.getOriginalFilename();
+		
+		dbProduct.setTitle(product.getTitle());
+		dbProduct.setDescription(product.getDescription());
+		dbProduct.setPrice(product.getPrice());
+		dbProduct.setStock(product.getStock());
+		
+		Product updateProduct = productRepository.save(dbProduct);
+		if(!ObjectUtils.isEmpty(updateProduct)) {
+			if(!image.isEmpty()) {
+				try {
+					File saveFile = new ClassPathResource("static/img").getFile();
+
+		            Path path = Paths.get(saveFile.getAbsolutePath()
+		                    + File.separator
+		                    + "product_img"
+		                    + File.separator
+		                    + imageName);
+
+		            Files.copy(image.getInputStream(), path,
+		                    StandardCopyOption.REPLACE_EXISTING);
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+			return product;
+		}
+		
+		
+		return null;
 	}
 }
