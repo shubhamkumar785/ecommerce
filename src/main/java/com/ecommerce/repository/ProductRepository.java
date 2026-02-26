6cbc3a154c32ac56683581a9192670c31901cb72
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-26 23:34:56 +0530

    Add method to find active products in repository
    Added method to fetch active products.

--- src/main/java/com/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.ecommerce.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ecommerce.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer>{
+
+	List<Product> findByIsActiveTrue();
 
 }
