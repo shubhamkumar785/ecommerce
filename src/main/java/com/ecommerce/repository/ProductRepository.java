dbc875285744d9a2cc98e71cc27deec8946f969b
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-27 16:37:56 +0530

    Add method to find products by category

--- src/main/java/com/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/repository/ProductRepository.java
@@ -1,12 +1,15 @@
 package com.ecommerce.repository;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ecommerce.model.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Integer>{
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 	List<Product> findByIsActiveTrue();
+
+	List<Product> findByCategory(String category);
 
 }
