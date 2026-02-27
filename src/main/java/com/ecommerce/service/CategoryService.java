59405093190bd69635d8b0bf605688942956fb38
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-27 16:39:09 +0530

    Remove unnecessary whitespace in CategoryService

--- src/main/java/com/ecommerce/service/CategoryService.java
+++ src/main/java/com/ecommerce/service/CategoryService.java
@@ -7,16 +7,15 @@ import com.ecommerce.model.Category;
 public interface CategoryService {
 
 	public Category saveCategory(Category category);
-	
-	public Boolean existCategory(String name);
-	
-	public List<Category> getAllCategory();
-	
-	public Boolean deleteCategory(int id);
-	
-	public Category getCategoryById(int id);
-	
-	public List<Category> getAllActiveCategory();
 
+	public Boolean existCategory(String name);
+
+	public List<Category> getAllCategory();
+
+	public Boolean deleteCategory(int id);
+
+	public Category getCategoryById(int id);
+
+	public List<Category> getAllActiveCategory();
+	
 }
- 
