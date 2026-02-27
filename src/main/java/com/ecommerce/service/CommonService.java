43ca393514597be88a75230d39c3ef07ac415f55
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-27 16:39:41 +0530

    Implement CommonService to manage session messages
    Implemented CommonService interface in CommnServiceImpl to remove session messages.

--- src/main/java/com/ecommerce/service/CommonService.java
+++ src/main/java/com/ecommerce/service/CommonService.java
@@ -1,9 +1,22 @@
 package com.ecommerce.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Service
-public interface CommonService {
+public class CommnServiceImpl implements CommonService {
 
-	public void removeSessionMessage();
+	@Override
+	public void removeSessionMessage() {
+		HttpServletRequest request = ((ServletRequestAttributes) (RequestContextHolder.getRequestAttributes()))
+				.getRequest();
+		HttpSession session = request.getSession();
+		session.removeAttribute("succMsg");
+		session.removeAttribute("errorMsg");
+	}
+
 }
