27bc4ced0ea091d9ba4d0a0d8872d802f121c024
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-27 16:41:29 +0530

    Refactor ProductService interface methods
    Updated method names for consistency and added category parameter to getAllActiveProducts.

--- src/main/java/com/ecommerce/service/ProductService.java
+++ src/main/java/com/ecommerce/service/ProductService.java
@@ -7,16 +7,17 @@ import org.springframework.web.multipart.MultipartFile;
 import com.ecommerce.model.Product;
 
 public interface ProductService {
-	
+
 	public Product saveProduct(Product product);
-	
-	public List<Product> getAllProduct();
+
+	public List<Product> getAllProducts();
 
 	public Boolean deleteProduct(Integer id);
-	
+
 	public Product getProductById(Integer id);
-	
+
 	public Product updateProduct(Product product, MultipartFile file);
 
-	public List<Product> getAllActiveProduct();
+	public List<Product> getAllActiveProducts(String category);
+	
 }
