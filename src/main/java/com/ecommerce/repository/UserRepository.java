ab9f3acfce2919191b300a660152be8f67d62144
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-28 12:20:15 +0530

    Enhance UserRepository with additional query methods
    Added methods to find users by email, role, and reset token.

--- src/main/java/com/ecommerce/repository/UserRepository.java
+++ src/main/java/com/ecommerce/repository/UserRepository.java
@@ -1,4 +1,5 @@
 package com.ecommerce.repository;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -6,4 +7,10 @@ import com.ecommerce.model.UserDtls;
 
 public interface UserRepository extends JpaRepository<UserDtls, Integer> {
 
+	public UserDtls findByEmail(String email);
+
+	public List<UserDtls> findByRole(String role);
+
+	public UserDtls findByResetToken(String token);
+	
 }
