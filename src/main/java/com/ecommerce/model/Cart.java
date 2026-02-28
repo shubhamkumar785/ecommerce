e337cc42f06dbaab37e8263c6e982b88e6515c0e
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-28 12:13:33 +0530

    Add files via upload

---
+++ src/main/java/com/ecommerce/model/Cart.java
@@ -0,0 +1,69 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Transient;
+
+@Entity
+public class Cart {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@ManyToOne
+	private UserDtls user;
+
+	@ManyToOne
+	private Product product;
+
+	private Integer quantity;
+
+	@Transient
+	private Double totalPrice;
+
+	// ===== Getter & Setter =====
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public UserDtls getUser() {
+		return user;
+	}
+
+	public void setUser(UserDtls user) {
+		this.user = user;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(Double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+}
