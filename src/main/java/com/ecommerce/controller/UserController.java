e4a6cb53931e7bf49405e798dcf50bae4e2ea455
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-02-28 12:08:34 +0530

    add user controller file

---
+++ src/main/java/com/ecommerce/controller/UserController.java
@@ -0,0 +1,64 @@
+package com.ecommerce.controller;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ecommerce.model.Cart;
+import com.ecommerce.model.Category;
+import com.ecommerce.model.UserDtls;
+import com.ecommerce.service.CartService;
+import com.ecommerce.service.CategoryService;
+import com.ecommerce.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private CategoryService categoryService;
+
+	@Autowired
+	private CartService cartService;
+
+	@GetMapping("/")
+	public String home() {
+		return "user/home";
+	}
+
+	@ModelAttribute
+	public void getUserDetails(Principal p, Model m) {
+		if (p != null) {
+			String email = p.getName();
+			UserDtls userDtls = userService.getUserByEmail(email);
+			m.addAttribute("user", userDtls);
+		}
+
+		List<Category> allActiveCategory = categoryService.getAllActiveCategory();
+		m.addAttribute("categorys", allActiveCategory);
+	}
+
+	@GetMapping("/addCart")
+	public String addToCart(@RequestParam Integer pid, @RequestParam Integer uid,HttpSession session) {
+		Cart saveCart = cartService.saveCart(pid, uid);
+		
+		if (ObjectUtils.isEmpty(saveCart)) {
+			session.setAttribute("errorMsg", "Product add to cart failed");
+		}else {
+			session.setAttribute("succMsg", "Product added to cart");
+		}
+		return "redirect:/product/" + pid;
+	}
+
+}
